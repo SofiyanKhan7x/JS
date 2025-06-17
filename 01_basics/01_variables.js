edc05112c9c69ec2e5a81b4a64a434879727db85
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-06-17 11:53:56 +0000

    let,const,var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accoutID = 1234
+let accountName = "Sufiyan khan"
+var account = "insta"
+accountOwner = "Neon"
+
+console.log(accoutID)
+console.table([account,accoutID,accountName,accountOwner]);
+
+/*
+
+Do not use var because its not provide any scope use let because its provide us scope
+*/
