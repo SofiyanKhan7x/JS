c010f26da67c1df1966ce81f9007a6088f5db019
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-07-01 11:45:38 +0000

    summary of datatypes

---
+++ 01_basics/datatypes_Summary.js
@@ -0,0 +1,32 @@
+let bigNumber = 93328983475934875
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id);
+// console.log(anotherId);
+// console.log(id === Symbol('123'));
+
+
+let funVaraibe = function(){
+    console.log("Hello World");
+    
+}
+
+// console.log(funVaraibe);
+
+// funVaraibe()
+
+let object = {
+    name : "Sufiyan Khan",
+    age : 21
+}
+
+// console.log( typeof object);
+
+let arrayInJs = ["Sufiyan khan",21];
+
+console.log(  typeof arrayInJs);
+
