00376c99cc2afb653cfe2f3a2c329eb951595ca8
SofiyanKhan7x <suffu7x@gmail.com>
2025-09-18 15:51:16 +0530

    Arrays in JS

---
+++ 01_basics/Arrays.js
@@ -0,0 +1,8 @@
+let names = ["sufiyan","mohammed","najeeb"];
+
+let unknown = [23,"hello",10.3];
+
+// console.log(names.concat(unknown));
+// console.log(names[2]);
+// console.log(names.length);
+console.log(names.reverse());
