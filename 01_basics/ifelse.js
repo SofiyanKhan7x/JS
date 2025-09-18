1a27f37ac3e8d7a77e985dcd95fdee9a4ebd5891
SofiyanKhan7x <suffu7x@gmail.com>
2025-09-18 16:37:37 +0530

    conditional statements and Loops

---
+++ 01_basics/ifelse.js
@@ -0,0 +1,15 @@
+const arr = ["sufiyan","neon","mohammed","md"];
+
+if(arr[0] == arr[1]){
+    console.log("they are same person ");
+    
+}else if (arr[2] == arr[3]) {
+    console.log("they are same person ");
+    
+} else {
+    
+}{
+       console.log("they are same person but the names are different!");
+
+    
+}
---
+++ 01_basics/loops.js
@@ -0,0 +1,25 @@
+let arr = ["suffu", "md","nv","najjo"];
+
+
+// for loop
+// for( let i =0; i < arr.length; i++){
+//     console.log(arr[i]);
+    
+// }
+
+
+// while loop
+
+// let i = 0;
+// while (i < arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+let i = 1;
+do{
+    console.log("runs atleast once");
+}while(i > arr.length){
+    console.log("condition is not matched");
+    
+}
