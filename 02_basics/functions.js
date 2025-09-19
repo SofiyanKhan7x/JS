370e5b260b34bd8430570c57463b739049bd99f8
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-09-19 11:21:30 +0000

    functions and datatypes basics

---
+++ 02_basics/functions.js
@@ -0,0 +1,23 @@
+const hero = ["Ironman","captain america","spider man"];
+
+const myobj ={
+    "name":"tony",
+    "age": 22,
+    "phone":8421234411
+}
+
+console.log(typeof hero);
+
+
+// let myFuntion = function(){
+//     console.log("Hello worlds",myobj.name);
+    
+// }
+
+// console.log(typeof myFuntion());
+
+
+
+// there are 2 type of datatypes in the javascript
+// Premitive datatype :  Stirng ,Number ,Boolean ,Undefined ,null ,symbol , NigInt
+// Non Premitive datatypes : Arrays , Objects ,  Functions
