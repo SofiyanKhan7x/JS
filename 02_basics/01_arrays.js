06df1f5081ae01c0fe524a6c81f4bb8d7925ced8
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-09-22 12:01:33 +0000

    array basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,20 @@
+const  arr = [12,"hello", 100.22,true];
+
+// console.log(arr[0]);
+
+// const arr2 = 
+// console.log( typeof arr);
+
+
+const arr2 = [0,1,2,3,4,5];
+
+// console.log("A = " , arr2);
+// console.log("slice ",arr2.slice(1,3) );   slice method can't manipulate the real Array
+// console.log("Aafter slice = " , arr2);
+
+// console.log("A = " , arr2);
+// console.log("slice ",arr2.splice(1,3) ); // Splice methid actully manipulate the real array
+// console.log("Aafter slice = " , arr2);
+
+console.log(arr2.length);
+
