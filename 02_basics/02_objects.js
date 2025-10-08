2fc1a2476a0404553f06a455746492264e82cca1
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-10-08 16:24:56 +0000

    basics of funtions

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,8 @@
+const obj = {
+    name : "sufiyan",
+    "full name" : "sufiyan khan",
+    email : "suffu7x@gmail.com"
+}
+
+console.log(obj["full name"]);
+console.log(obj.email);
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+// function sayMyName(){
+//     console.log("Hey suffu!");
+    
+// }
+
+function addTwoNumber(number1 , number2){
+    const result = number1 + number2;
+    return result
+}
+
+
+console.log(addTwoNumber(2,3));
+
