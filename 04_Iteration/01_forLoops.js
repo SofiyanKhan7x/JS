1c1e38995f943ebc47b4d1d3a322d6060947f3bf
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-10-25 12:02:03 +0000

    for loop with break and continue keyword

--- 04_Iteration/01_forLoops.js
+++ 04_Iteration/01_forLoops.js
@@ -1,5 +1,25 @@
-for (let index = 0; index < 10; index++) {
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+    
+// }
+
+
+// for (let index = 1; index <= 10; index++) {
+//     const element = index;
+//     if(element == 5){
+        // break
+//     }
+//     console.log(element);
+    
+    
+// }
+for (let index = 1; index <= 10; index++) {
     const element = index;
+    if(element == 5){
+        continue
+    }
     console.log(element);
     
     
