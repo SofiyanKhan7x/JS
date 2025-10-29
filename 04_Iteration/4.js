cc953697b6235b3f9b94e269a912a05d8337ba62
Momin Sofiyan Khan <154498433+SofiyanKhan7x@users.noreply.github.com>
2025-10-29 14:47:56 +0000

    use of reduce function

---
+++ 04_Iteration/4.js
@@ -0,0 +1,21 @@
+const arr = [
+    {
+        itemName: "js course",
+        price : 605
+    },
+    {
+        itemName: "java course",
+        price : 999
+    },
+    {
+        itemName: "py course",
+        price : 399
+    },
+]
+
+
+const newArr = arr.reduce( (acc ,curval) => {
+    return acc  + curval.price
+},0 )
+
+console.log(newArr);
